6ccb22ab6fd7d09ef7036710d2158407c0c53d3f
yeonfish6040 <yeonfish6040@gmail.com>
2023-01-13 21:51:43 +0900

    [안미정] - mybatis INSERT 예제 풀이

--- src/main/java/com/duboomom/spring/ex/mybatis/ReviewController.java
+++ src/main/java/com/duboomom/spring/ex/mybatis/ReviewController.java
@@ -10,6 +10,7 @@ import com.duboomom.spring.ex.mybatis.bo.ReviewBO;
 import com.duboomom.spring.ex.mybatis.model.Review;
 
 @Controller
+@RequestMapping("/mybatis")
 public class ReviewController {
 	
 	@Autowired
@@ -17,7 +18,7 @@ public class ReviewController {
 	
 	// id가 3인 데이터 조회
 	@ResponseBody
-	@RequestMapping("/mybatis/ex01")
+	@RequestMapping("/ex01")
 	public Review review(
 			@RequestParam("id") int id
 //			@RequestParam(value="id", required=false) int id
@@ -28,5 +29,26 @@ public class ReviewController {
 		
 		return review;
 	}
+	
+	
+	// insert
+	@ResponseBody
+	@RequestMapping("/ex02")
+	public String addReview() {
+//		int count = reviewBO.addReview(4, "콤비네이션피자", "안미정", 4.5, "할인도 많이 받고 잘 먹었습니다.");
+		
+		Review review = new Review();
+		review.setStoreId(2);
+		review.setMenu("뿌링클");
+		review.setUserName("안미정");
+		review.setPoint(4.0);
+		review.setReview("역시 뿌링클이 진리입니다!!");
+				
+		int count = reviewBO.addReviewByObject(review);
+		
+		return "삽입결과 " + count;
+		
+	}
+	
 	
 }
