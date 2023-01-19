c987e5ae1260b7cc81f91d0f7a960e963175d100
yeonfish6040 <yeonfish6040@gmail.com>
2023-01-19 21:50:05 +0900

    [안미정] - jstl 코어라이브러리 반복문 조건문 - jstl 포맷라이브러리

--- src/main/java/com/duboomom/spring/ex/jstl/JSTLController.java
+++ src/main/java/com/duboomom/spring/ex/jstl/JSTLController.java
@@ -1,6 +1,13 @@
 package com.duboomom.spring.ex.jstl;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,5 +19,55 @@ public class JSTLController {
 	public String ex01() {
 		return "jstl/ex01";
 	}
+	
+	@GetMapping("/ex02")
+	public String ex02(Model model) {
+		
+		Integer weight = 75;
+		model.addAttribute("weight", weight);
+		
+		List<String> fruitList = new ArrayList<>();
+		fruitList.add("사과");
+		fruitList.add("딸기");
+		fruitList.add("바나나");
+		
+		model.addAttribute("fruitList", fruitList);
+		
+		// List<map>
+		// 사용자 정보 Map
+		
+		List<Map<String, Object>> userList = new ArrayList<>();
+		
+		Map<String, Object> map = new HashMap<>();
+		map.put("name", "안미정");
+		map.put("age", 20);
+		map.put("hobby", "댄스");
+		userList.add(map);
+		
+		map = new HashMap<>();
+		map.put("name", "안두부");
+		map.put("age", 2);
+		map.put("hobby", "사냥");
+		userList.add(map);
+		
+		model.addAttribute("userList", userList);		
+			
+			
+		return "jstl/ex02";
+	}
+	
+	@GetMapping("/ex03")
+	public String ex03(Model model) {
+		
+		Date now = new Date();
+		model.addAttribute("now", now);
+		
+		String dateString = "2023/01/19 21:44:10";
+		model.addAttribute("dateString", dateString);
+		
+		return "jstl/ex03";
+	}
+	
+	
 	
 }
